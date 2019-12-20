8e66df6c65384c99dd315ac0073d673fbe7fc4ab
dandip <dandi.pangestu@quadras.co.id>
2019-12-20 18:04:54 +0700

    Menambahkan Halaman Pengaturan

---
+++ src/components/AppPageLoading/index.js
@@ -0,0 +1,19 @@
+import React from 'react';
+
+// Material UI
+import CircularProgress from '@material-ui/core/CircularProgress';
+
+import useStyles from './styles';
+
+function AppPageLoading() {
+
+    const classes = useStyles();
+
+    return (
+            <div className={classes.loadingBox}>
+                <CircularProgress />
+            </div>
+    )
+}
+
+export default AppPageLoading;
---
+++ src/pages/private/pengaturan/styles/pengguna.js
@@ -0,0 +1,12 @@
+import { makeStyles } from '@material-ui/core/Styles';
+
+
+const useStyles = makeStyles(theme => ({
+    pengaturanPengguna: {
+        display: 'flex',
+        flexDirection: 'column',
+        width:300
+    }
+}))
+
+export default useStyles;
